ea4a4d54fc3b25f44b483de13f7c481cdd5c1baa
ali <github@alihong.net>
2020-04-17 17:45:51 +0000

    day01p02

--- src/day01.rs
+++ src/day01.rs
@@ -3,12 +3,30 @@ use std::fs;
 fn part_one() -> i32 {
     get_input()
         .lines()
-        .map(|m| m.parse::<i32>().unwrap() / 3 - 2)
+        .map(|m| m.parse::<i32>().unwrap())
+        .map(|m| m / 3 - 2)
         .sum()
 }
 
 fn get_input() -> String {
     fs::read_to_string("inputs/day01.txt").unwrap()
+}
+
+fn calculate_fuel(mass: i32) -> i32 {
+    let fuel = mass / 3 - 2;
+    if fuel <= 0 {
+        return 0
+    } else {
+        return fuel + calculate_fuel(fuel)
+    }
+}
+
+fn part_two() -> i32 {
+    get_input()
+        .lines()
+        .map(|m| m.parse::<i32>().unwrap())
+        .map(|m| calculate_fuel(m))
+        .sum()
 }
 
 #[cfg(test)]
@@ -23,5 +41,10 @@ mod tests {
     #[test]
     fn test_part_one() {
         assert_eq!(part_one(), 3126794);
+    }
+
+    #[test]
+    fn test_part_two() {
+        assert_eq!(part_two(), 4687331);
     }
 }
